51e703d0aa196e1ef5351ddcc5c6dc7de30f7f10
Injamul Haque Somrat <ihsomrat.official@gmail.com>
2026-01-11 19:49:42 +0600

    add: added user creation primarily from better auth

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express, { Application } from "express";
+import { toNodeHandler } from "better-auth/node";
 import { postRouter } from "./modules/post/post.router";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
